667f0252be8247e953051740840660217ed7e8c2
wellijo18 <144364062+wellijo18@users.noreply.github.com>
2025-03-04 21:17:57 -0500

    Add files via upload

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,26 +1,35 @@
 import Layout from '../layout/layout';
+import image404 from "@/assets/404.png";
+import React from "react";
 
-const About = () => {
+export default function About() {
     return (
-        <Layout>
-            <div style={{ padding: '2rem' }}>
-                <h1>About Space News</h1>
-                <p>
-                    Welcome to Space News! This website provides the latest news and updates
-                    about space exploration, astronomy, and related topics.
-                </p>
-                <p>
-                    Our content is sourced from the Spaceflight News API, bringing you
-                    up-to-date information about space missions, discoveries, and advancements
-                    in space technology.
-                </p>
-                <p>
-                    This website was developed as part of a web development course assignment
-                    using Next.js and Ant Design.
-                </p>
+        <>
+            <div style={{display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center", padding: "2rem" }}>
+                <div style={{marginBottom: "2rem"}}>
+                    <h1>Wellington Oliveira</h1>
+                </div>
+                <div style={{display: "flex", alignItems: "center", gap: "2rem"}}>
+                    <img
+                        src={"../welli.png"}
+                        alt={"Picture of Author"}
+                        onError={(e) => (e.currentTarget.src = image404.src)}
+                        style={{
+                            width: "18vw",
+                            height: "18vw",
+                        }}
+                    />
+                    <p style={{maxWidth: "20vw"}}> Wellington is current a senior studying computer science at Boston University. He currently works at Apple and is
+                        hoping to further his development skills! He decided he wants to share some news articles with the world, find out more about the website</p>
+                </div>
+                <div style={{marginTop: "2rem" }}>
+                    <a href="/" style={{ textDecoration: "none", color: "blue", fontSize: "2rem"}}>
+                        Click for our Space News Site!
+                    </a>
+                </div>
             </div>
-        </Layout>
+
+        </>
+
     );
 };
-
-export default About;
